6177187ee85464122f4d5370ffdf010d43b9d3f9
Alienware <trongnd02@gmail.com>
2024-11-27 23:52:56 +0700

    update setup

--- Web/test/tmp.py
+++ Web/test/tmp.py
@@ -1,28 +1,27 @@
-# from fastapi import FastAPI
-# import gradio as gr
-# 
-# CUSTOM_PATH_1 = "/gradio_1"
-# CUSTOM_PATH_2 = "/gradio_2"
-# 
-# app = FastAPI()
-# 
-# 
-# @app.get("/")
-# def read_main():
-#     return {"message": "This is your main app"}
-# 
-# 
-# io1 = gr.Interface(lambda x: "Hello, " + x + "!", "textbox", "textbox")
-# io2 = gr.Interface(lambda x: "Hello new world, " + x + "!", "textbox", "textbox")
-# 
-# app = gr.mount_gradio_app(app, io1, path=CUSTOM_PATH_1)
-# app = gr.mount_gradio_app(app, io2, path=CUSTOM_PATH_2)
+import uvicorn
+import gradio as gr
+from fastapi import FastAPI
 
-import torch
+CUSTOM_PATH_1 = "/gradio_1"
+CUSTOM_PATH_2 = "/gradio_2"
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_main():
+    return {"message": "This is your main app"}
+
+
+io1 = gr.Interface(lambda x: "Hello, " + x + "!", "textbox", "textbox")
+io2 = gr.Interface(lambda x: "Hello new world, " + x + "!", "textbox", "textbox")
+
+app = gr.mount_gradio_app(app, io1, path=CUSTOM_PATH_1)
+app = gr.mount_gradio_app(app, io2, path=CUSTOM_PATH_2)
 
 
 def main() -> None:
-    print(torch.cuda.is_available())
+    uvicorn.run("tmp:app", host="127.0.0.1", port=6969, log_level="info", timeout_keep_alive=1)
     return None
 
 
