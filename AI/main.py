cb28af176e95c6c4bc3b5cd59682ffa848159aaa
Alienware <trongnd02@gmail.com>
2024-11-30 19:33:26 +0700

    update setup

---
+++ AI/main.py
@@ -0,0 +1,31 @@
+"""
+API for model will be defined here.
+"""
+import torch
+import uvicorn
+from fastapi import FastAPI, Response
+
+app = FastAPI()
+
+
+@app.get("/health-check")
+def check_health():
+    return {
+        "exit code": Response(status_code=200).status_code,
+    }
+
+
+@app.get("/cuda-check")
+def check_cuda():
+    return {
+        "Cuda status": torch.cuda.is_available(),
+    }
+
+
+def main() -> None:
+    uvicorn.run(app, host="localhost", port=6968)
+    return None
+
+
+if __name__ == '__main__':
+    main()
