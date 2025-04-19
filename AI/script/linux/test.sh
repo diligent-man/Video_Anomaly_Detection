02cda7dfc29ab67c991f3158c04b9d5ce190b3e6
Alienware <trongnd02@gmail.com>
2025-04-20 00:05:24 +0700

    update test.py

--- AI/script/linux/test.sh
+++ AI/script/linux/test.sh
@@ -1,3 +1,5 @@
-python3 ../../src/tools/test.py --config ../../config/single/rgb_2D.json
-#python3 ../../src/tools/test.py --config ../../config/single/rgb_3D.json
-#python3 ../../src/tools/test.py --config ../../config/single/rgb_mixed.json
+python3 ../../src/tools/test.py \
+    --device "cuda" \
+    --config  /home/trong/Downloads/Local/Source/Python/semester_9/AIP391/Video_anomaly_detection/AI/results/final_train_result/teacher/input_3d/v1/Mlflow/890005033899140195/6653ca3fc5da40cbaf0101124bac5d70/artifacts/config.json \
+    --resume_ckpt /home/trong/Downloads/Local/Source/Python/semester_9/AIP391/Video_anomaly_detection/AI/results/final_train_result/teacher/input_3d/v1/ckpt/best_epoch14_step3163.pt \
+    --log_path /home/trong/Downloads/Local/Source/Python/semester_9/AIP391/Video_anomaly_detection/AI/results/final_train_result/teacher/input_3d/v1/log
--- AI/script/linux/train.sh
+++ AI/script/linux/train.sh
@@ -2,9 +2,9 @@
 # Should run with ./train.sh
 
 # train teacher
-#python3 ../../src/tools/train.py --config ../../config/single/rgb_2D.json
-python3 ../../src/tools/train.py --config ../../config/single/rgb_3D.json
-#python3 ../../src/tools/train.py --config ../../config/single/rgb_mixed.json
+python3 ../../src/tools/train.py --config ../../config/single/rgb_2D.json && \
+python3 ../../src/tools/train.py --config ../../config/single/rgb_3D.json && \
+python3 ../../src/tools/train.py --config ../../config/single/rgb_mixed.json
 
 # train student
 #python3 ../../src/tools/train.py --config ../../config/distillation/rgb_2D.json
