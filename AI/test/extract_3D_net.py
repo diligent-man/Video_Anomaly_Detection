ac9bf52150f498dd24204c291d7f8a212e1f302c
Alienware <trongnd02@gmail.com>
2025-05-10 11:14:46 +0700

    change name ModelForwarder -> MultiBackboneForwarder, rm ./AI/src/callbacks/base/

--- AI/test/extract_3D_net.py
+++ AI/test/extract_3D_net.py
@@ -12,7 +12,7 @@ from AI.src.utils import (
     create_feature_extractor
 )
 from AI.src.modeling.backbones import (
-    ModelForwarder,
+    MultiBackboneForwarder,
     NET_3D_REDUCE,
     DEFAULT_3D_REDUCE,
     NET_DEFAULT_CONFIG,
@@ -73,7 +73,7 @@ def main() -> None:
                 model.compile()
 
             x: torch.Tensor = torch.rand((B, S, C, T, *dummy_input[-2:]), device=device, dtype=torch.float16)
-            model_forwarder = ModelForwarder(model, name, NET_3D_REDUCE[DEFAULT_3D_REDUCE])
+            model_forwarder = MultiBackboneForwarder(model, name, NET_3D_REDUCE[DEFAULT_3D_REDUCE])
 
             features = model_forwarder(x)
 
